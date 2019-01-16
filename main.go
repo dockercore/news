0443566cfa5ca46c610b8a3658a8e960c88b89d4
dockercore <790160000@qq.com>
2019-01-16 11:44:24 +0800

    假装在提交

--- main.go
+++ main.go
@@ -6,6 +6,33 @@ import (
 )
 
 func main() {
+
+	//beego.AddFuncMap("hi",hello)
+	beego.AddFuncMap("showprepage",prepage)
+	beego.AddFuncMap("shownextpage",shownextpage)
 	beego.Run()
 }
+
+
+//上一页函数
+func prepage(pageindex int)(preIndex int){
+	preIndex= pageindex-1
+	return
+}
+
+//下一页函数
+func shownextpage(pageindex int)(nextIndex int){
+	nextIndex = pageindex +1
+	return
+}
+
+
+
+/*
+//html里面调用
+func hello(in string)(out string){
+	out = in + "world"
+	return
+}
+*/
 
--- models/model.go
+++ models/model.go
@@ -11,24 +11,28 @@ type User struct {
 	Name string
 	Pwd string
 	//Age string
+
+	Article []*Article `orm:"rel(m2m)"`
 }
 
 type Article struct {
-	Id int   `orm:"pk;auto"` //主键 并且自动增长
+	Id int `orm:"pk;auto"` //主键 并且自动增长
 	ArtiName string `orm:"size(20)"` //ArtiName长度为20
-	 Atime time.Time `orm:"auto_now"`
+	Atime time.Time `orm:"auto_now"`
 	Acount int `orm:"default(0);null"`
 	Acontent string
 	Aimg string
+
+	User []*User `orm:"reverse(many)"`
 }
+
+
 
 func init()  {
 	//设置数据库的基本信息
-	orm.RegisterDataBase("default","mysql","root:123@tcp(127.0.0.1:3306)/test5?charset=utf8")
+	orm.RegisterDataBase("default","mysql","root:root@tcp(127.0.0.1:3306)/test5?charset=utf8")
 	//映射modle数据
 	orm.RegisterModel(new(User),new(Article))
 	//生成表
 	orm.RunSyncdb("default",false,true)
 }
-
-
